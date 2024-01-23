47965ca45055fdd14fd1880d2fa7a147ae431add
spring <aaa@aa.com>
2024-01-23 14:25:31 +0900

    2nd

--- controller.py
+++ controller.py
@@ -3,18 +3,23 @@ app = f.Flask(__name__)
 
 @app.route("/")
 def 목록():
-  pass
+  result = dao.findall()
+  return f.render_template("list.html", list=result)
 
 @app.route("/read")
 def 읽기():
-  pass
+  return f.render_template("read.html")
 
 @app.route("/write")
 def 쓰기_화면():
-  pass
+  return f.render_template("write.html")
 
 @app.route("/write", methods=['post'])
 def 쓰기():
-  pass
+  title = f.request.form.get("title", type=str)
+  content = f.request.form.get("content", type=str)
+  nickname = f.request.form.get("nickname", type=str)
+  dao.save(title=title, content=content, nickname=nickname)
+  return f.redirect("/")
 
 app.run(debug=True)
