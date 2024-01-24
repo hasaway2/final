634b99aee72fdefa7e22b87c4d49dfe1a861f7f2
spring <aaa@aa.com>
2024-01-24 10:16:25 +0900

    dao 작성

--- dao.py
+++ dao.py
@@ -1,10 +1,9 @@
-board_list = []
-print("1111111111111111111")
-board_list.append(dict(bno=1,title="aa",content="aaa",nickname='spring',readcnt=0))
-board_list.append(dict(bno=2,title="bb",content="bbb",nickname='spring',readcnt=0))
-board_list.append(dict(bno=3,title="cc",content="ccc",nickname='spring',readcnt=0))
-print(board_list)
-bno = 4
+# 데이터 : model
+# 모델을 다루는 클래스 : Data Access Object(DAO, 다오)
+board_list=[]
+bno = 1
+# 추가(save), 전체출력(findall), 하나출력(findone), 변경(update), 삭제(delete)
+# 글 : 글번호, 제목, 내용, 닉네임, 조회수
 
 def save(title:str, content:str, nickname:str)->bool:
   global bno
@@ -13,12 +12,31 @@ def save(title:str, content:str, nickname:str)->bool:
   bno+=1
   return True
 
-def findall()->list:
-  print(board_list)
+def findall():
   return board_list
 
+# findone, update, delete는 일단 찾는다
+# findone은 글을 찾아서 출력한다. 따라서 조회수도 증가해야 한다
 def findone(bno:int)->dict:
   for board in board_list:
     if board['bno']==bno:
+      board['readcnt']+=1
       return board
   return None
+
+# 변경은 찾아서 제목과 내용을 바꾼다
+def update(bno:int, title:str, content:str)->bool:
+  for board in board_list:
+    if board['bno']==bno:
+      board['title'] = title
+      board['content'] = content
+      return True
+  return False
+
+# 삭제
+def delete(bno:int)->bool:
+  for board in board_list:
+    if board['bno']==bno:
+      board_list.remove(board)
+      return True
+  return False
