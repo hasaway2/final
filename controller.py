4ac743087b4ee626f7e1944b01064d666e82ec2d
spring <aaa@aa.com>
2024-01-24 11:08:06 +0900

    controller 완성

--- controller.py
+++ controller.py
@@ -1,29 +1,55 @@
 import flask as f
 import dao
+# Create  get/post    
+# Read    get  
+# Update  get(/read)/post
+# Delete  get(/read)/post
+
+# get   html을 보여준다
+# post  처리 후 이동한다
 app = f.Flask(__name__)
 
 @app.route("/")
-def 목록():
-  result = dao.findall()
-  return f.render_template("list.html", list=result)
+def list() :
+  board_list = dao.findall()
+  return f.render_template("list.html", list=board_list)
 
-# /read?bno=1 또는 /read?bno=2
-@app.route("/read")
-def 읽기():
-  bno = f.request.args.get("bno", type=int)
-  board = dao.findone(bno)
-  return f.render_template("read.html", board=board)
-
-@app.route("/write")
-def 쓰기_화면():
+@app.route('/write')
+def 쓰기화면보여주기():
   return f.render_template("write.html")
 
 @app.route("/write", methods=['post'])
-def 쓰기():
-  title = f.request.form.get("title", type=str)
-  content = f.request.form.get("content", type=str)
-  nickname = f.request.form.get("nickname", type=str)
-  dao.save(title=title, content=content, nickname=nickname)
+def 쓰기처리():
   return f.redirect("/")
 
-app.run(debug=True)
+@app.route("/read")
+def 읽기():
+  bno = f.request.form.get('bno', type=int)
+  board = dao.findone(bno)
+  return f.render_template("read.html")
+
+# /read에서 내용을 바꾸고 변경버튼 클릭
+@app.route("/update", methods=['post'])
+def 변경():
+  bno = f.request.form.get("bno", type=int)
+  title = f.request.form.get("title", type=str)
+  content = f.request.form.get("content", type=str)
+  dao.update(bno=bno, title=title, content=content)
+  return f.redirect("/")
+
+# /read에서 삭제버튼 클릭
+@app.route("/delete", methods=['post'])
+def 삭제():
+  bno = f.request.form.get("bno", type=int)
+  dao.delete(bno=bno)
+  return f.redirect("/")
+
+
+
+
+
+
+
+
+
+
